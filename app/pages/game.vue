cb797bc0d894aa6ab05c7849fac64a7fcc626207
Tijmen Roes <roes.tijmen@gmail.com>
2026-04-06 22:44:08 +0700

    fix initial words set

--- app/pages/game.vue
+++ app/pages/game.vue
@@ -57,57 +57,57 @@
 </template>
 
 <script setup lang="ts">
-import { useWordsStore } from "~/stores/words";
+import { useWordsStore } from '~/stores/words'
 
-const TIMER_START = 30;
-const wordsStore = useWordsStore();
-wordsStore.checkWords();
+const TIMER_START = 30
+const wordsStore = useWordsStore()
+wordsStore.checkWords()
 
-const timer = ref(TIMER_START);
-const isTimerStarted = ref(false);
-const isTimerFinished = ref(false);
+const timer = ref(TIMER_START)
+const isTimerStarted = ref(false)
+const isTimerFinished = ref(false)
 
-const selectedWords = ref<string[]>([]);
-const score = ref(0);
-const amountRounds = ref(0);
+const selectedWords = ref<string[]>([])
+const score = ref(0)
+const amountRounds = ref(0)
 
-const rndWordList = ref<string[]>([]);
+const rndWordList = ref<string[]>(wordsStore.getRandomWords(5))
 const hintText = computed(() => {
-  return !isTimerFinished.value ? "Guess the following words" : "Time's up!";
-});
+  return !isTimerFinished.value ? 'Guess the following words' : "Time's up!"
+})
 
 function startTimer() {
-  timer.value = TIMER_START;
-  isTimerStarted.value = true;
-  isTimerFinished.value = false;
-  selectedWords.value = [];
+  timer.value = TIMER_START
+  isTimerStarted.value = true
+  isTimerFinished.value = false
+  selectedWords.value = []
   const interval = setInterval(() => {
-    timer.value--;
+    timer.value--
     if (timer.value <= 0) {
-      isTimerFinished.value = true;
-      clearInterval(interval);
+      isTimerFinished.value = true
+      clearInterval(interval)
     }
-  }, 1000);
+  }, 1000)
 }
 
 function handleWordClick(word: string) {
-  if (!isTimerFinished.value) return;
+  if (!isTimerFinished.value) return
   if (selectedWords.value.includes(word)) {
-    selectedWords.value = selectedWords.value.filter((w) => w !== word);
+    selectedWords.value = selectedWords.value.filter((w) => w !== word)
   } else {
-    selectedWords.value.push(word);
+    selectedWords.value.push(word)
   }
 }
 
 function selectAllWords() {
-  selectedWords.value = [...rndWordList.value];
+  selectedWords.value = [...rndWordList.value]
 }
 
 function submitScore() {
-  score.value += selectedWords.value.length;
-  isTimerStarted.value = false;
-  amountRounds.value++;
-  wordsStore.removeWords(rndWordList.value);
-  rndWordList.value = wordsStore.getRandomWords(5);
+  score.value += selectedWords.value.length
+  isTimerStarted.value = false
+  amountRounds.value++
+  wordsStore.removeWords(rndWordList.value)
+  rndWordList.value = wordsStore.getRandomWords(5)
 }
 </script>
