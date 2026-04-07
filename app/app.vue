f89cbfa023602441a823bd7459ebe65ea76c4755
Tijmen Roes <roes.tijmen@gmail.com>
2026-04-07 19:41:01 +0700

    add the head updates to app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,25 +1,25 @@
 <script setup>
+const config = useRuntimeConfig()
 useHead({
-  meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
-  link: [{ rel: "icon", href: "/favicon.ico" }],
+  meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+  link: [{ rel: 'icon', href: '/favicon.ico' }],
   htmlAttrs: {
-    lang: "en",
-  },
-});
+    lang: 'en'
+  }
+})
 
-const title = "Nuxt Starter Template";
-const description =
-  "A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.";
+const title = config.public.siteName
+const description = config.public.siteDescription
 
 useSeoMeta({
   title,
   description,
   ogTitle: title,
   ogDescription: description,
-  ogImage: "https://ui.nuxt.com/assets/templates/nuxt/starter-light.png",
-  twitterImage: "https://ui.nuxt.com/assets/templates/nuxt/starter-light.png",
-  twitterCard: "summary_large_image",
-});
+  ogImage: '/favicon.svg',
+  twitterImage: '/favicon.svg',
+  twitterCard: 'summary_large_image'
+})
 </script>
 
 <template>
